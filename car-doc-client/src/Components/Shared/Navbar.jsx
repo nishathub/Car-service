896466ed7780a4cf873a5ff2b80dc0fc625180b6
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2024-05-31 09:09:45 +0600

    navbar design complete with responsive

---
+++ car-doc-client/src/Components/Shared/Navbar.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { RiShoppingBagLine } from "react-icons/ri";
+import { CiSearch } from "react-icons/ci";
+
+
+
+const Navbar = () => {
+    const links =
+        <>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/'}>About</Link></li>
+            <li><Link to={'/'}>Service</Link></li>
+            <li><Link to={'/'}>Blog</Link></li>
+            <li><Link to={'/'}>Contact</Link></li>
+
+        </>
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {links}
+                        </ul>
+                    </div>
+                    <Link><img className="w-8 md:w-16" src="/logo.svg" alt="company-logo" /></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <Link to={'/'} className="btn btn-ghost text-lg"><RiShoppingBagLine/></Link>
+                    <button className="btn btn-ghost text-lg text-white"><CiSearch/></button>
+                    <Link to={'/'} className="btn btn-outline btn-accent">Appointment</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- car-doc-client/src/Roots/Roots.jsx
+++ car-doc-client/src/Roots/Roots.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Shared/Footer";
+import Navbar from "../Components/Shared/Navbar";
 
 const Roots = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
