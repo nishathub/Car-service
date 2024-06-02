9d6e05030b6a9d3ec3b25d665359a772ede55116
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2024-06-02 08:23:47 +0600

    contact section complete with mobile responsive

--- car-doc-client/src/Components/HomePageComponents/ContactCard.jsx
+++ car-doc-client/src/Components/HomePageComponents/ContactCard.jsx
@@ -1,8 +1,33 @@
+import { SlCalender } from "react-icons/sl";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+
+
 
 const ContactCard = () => {
     return (
-        <div>
-            
+        <div className="bg-base-300 lg:h-60 grid lg:grid-cols-3 items-center justify-items-center gap-4 py-4">
+            <div className="w-80 p-4 flex gap-4 items-center bg-base-100 rounded-lg">
+                <div className="text-accent"><SlCalender /></div>
+                <div>
+                    <p>We are open monday-friday</p>
+                    <p className="text-lg font-bold">7:00 am - 9:00 pm</p>
+                </div>
+            </div>
+            <div className="w-80 p-4 flex gap-4 items-center bg-base-100 rounded-lg">
+                <div className="text-accent"><FaPhoneAlt /></div>
+                <div>
+                    <p>Hava a question?</p>
+                    <p className="text-lg font-bold">+2546 342 2398</p>
+                </div>
+            </div>
+            <div className="w-80 p-4 flex gap-4 items-center bg-base-100 rounded-lg">
+                <div className="text-accent"><FaLocationDot /></div>
+                <div>
+                    <p>Need a repair? out address</p>
+                    <p className="text-lg font-bold">Liza Street, New York</p>
+                </div>
+            </div>
         </div>
     );
 };
--- car-doc-client/src/Pages/Home.jsx
+++ car-doc-client/src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import ContactCard from "../Components/HomePageComponents/ContactCard";
 import HomeAbout from "../Components/HomePageComponents/HomeAbout";
 import HomeBanner from "../Components/HomePageComponents/HomeBanner";
 import ServicesComponent from "../Components/HomePageComponents/ServicesComponent";
@@ -6,14 +7,17 @@ const Home = () => {
 
     return (
         <div className="">
-            <div className="lg:pb-12">
+            <div className="pb-4 lg:pb-12">
                 <HomeBanner></HomeBanner>
             </div>
-            <div className="py-12">
+            <div className="py-4 lg:py-12">
                 <HomeAbout></HomeAbout>
             </div>
-            <div className="py-12">
+            <div className="py-4 lg:py-12">
                 <ServicesComponent></ServicesComponent>
+            </div>
+            <div className="py-4 lg:py-12">
+                <ContactCard></ContactCard>
             </div>
         </div>
     );
